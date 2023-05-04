43e1d4f92f1b0db87ffb0ed81d64f35f96907d7a
Arr <Arromaev@gmail.com>
2023-05-04 14:28:34 +0500

    Реализация задания 2.2.1 по работе с БД через Hibernate

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,20 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      user1.setCar(new Car("lada", 123));
+      user2.setCar(new Car("lada", 321));
+      user3.setCar(new Car("Honda", 456));
+      user4.setCar(new Car("Civic", 777));
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +36,17 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getCar());
          System.out.println();
       }
+
+      System.out.println("Поиск по модели и серии:");
+      System.out.println();
+      System.out.println(userService.findUserByCar("lada", 123).toString());
+      System.out.println();
+      System.out.println(userService.findUserByCar("Honda", 456).toString());
+
+
 
       context.close();
    }
